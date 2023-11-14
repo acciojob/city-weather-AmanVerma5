c023c1e8a82ed645b8680aaa96fe4b55a0a63b1c
AmanVerma5 <amanv4775@gmail.com>
2023-11-14 14:51:00 +0530

    submitting code-Tue Nov 14 2023 14:50:57 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,54 @@
 
-import React from "react";
+import React, { useState,useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [search,setSearch]=useState();
+  const [name,setName]=useState();
+  const [temp,setTemp]=useState();
+  const [weather,setWeather]=useState();
+  const [icon,setIcon]=useState();
+  const [loading,setLoading]=useState(true);
+  
+
+     const debounce = (func, delay) => { 
+        let debounceTimer 
+        return function() { 
+            const context = this
+            const args = arguments 
+                clearTimeout(debounceTimer) 
+                    debounceTimer 
+                = setTimeout(() => func.apply(context, args), delay) 
+        } 
+    } 
+
+  useEffect(debounce(function(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=927107ad549126b6f6d80e3a37255431`)
+    .then((response)=>response.json())
+    .then((jsonData)=>{
+      setName(jsonData.name);
+      setTemp(jsonData.main.temp)
+      setWeather(jsonData.weather[0].main)
+      setIcon(jsonData.weather[0].icon)
+      setLoading(false)
+      console.log(jsonData)})
+    .catch((error)=>console.log(error))
+  },3000),[search])
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input className="search" type='text' value={search} onChange={(e)=>setSearch(e.target.value)}/>
+        <div className="weather">
+          {
+            loading?<>
+            
+            </>:<>
+            <div>{name}</div>
+            <div>{temp}</div>
+            <div>{weather}</div>
+            <div>{icon}</div>
+            </>
+          }
+        </div>
     </div>
   )
 }
